21d1b4cfd9892d4ce4c73d844d121aab70c10dca
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2020-09-28 17:49:11 -0500

    Actividad para lectura de tipo de dato y mostrar tipo de dato

--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+
+    getline(cin,cadena);
+
+    if(cadena=="int"){
+        cout<<sizeof(int);
+    }
+    else if(cadena=="float"){
+        cout<<sizeof(float);
+    }
+    else if(cadena=="short"){
+        cout<<sizeof(short);
+    }
+    else if(cadena=="long"){
+        cout<<sizeof(long);
+    }
+    else if(cadena=="char"){
+        cout<<sizeof(char);
+    }
+    else if(cadena=="double"){
+        cout<<sizeof(double);
+    }
 
     return 0;
 }
